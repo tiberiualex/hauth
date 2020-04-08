0e1fb673c043eb083fd39f9a15f59e9cf93d4906
tiberiualex <tiberiu.alexander@gmail.com>
2020-04-08 23:43:38 +0300

    Add types and functions for registration and auth flow

--- src/Domain/Auth.hs
+++ src/Domain/Auth.hs
@@ -3,6 +3,7 @@ module Domain.Auth where
 import ClassyPrelude
 import Domain.Validation
 import Text.Regex.PCRE.Heavy
+import Control.Monad.Except
 
 data RegistrationError
   = RegistrationErrorEmailTaken
@@ -46,3 +47,79 @@ data Auth = Auth
   { authEmail :: Email
   , authPassword :: Password
   } deriving (Show, Eq)
+
+
+-- Unfortunately the file structure is not explained very well, so unsure if
+-- some of these declarations are supposed to be here or in a different file
+type VerificationCode = Text
+
+-- so our repo needs to implement this function that takes an Auth "object"
+-- and returns either a registration error or the verification code
+class Monad m => AuthRepo m where
+  addAuth :: Auth -> m (Either RegistrationError VerificationCode)
+  setEmailAsVerified :: VerificationCode -> m (Either EmailVerificationError ())
+  findUserByAuth :: Auth -> m (Maybe (UserId, Bool))
+  findEmailFromUserId :: UserId -> m (Maybe Email)
+
+class Monad m => EmailVerificationNotif m where
+  notifyEmailVerification :: Email -> VerificationCode -> m ()
+
+{- 
+  ExceptT is a monad transformer that adds exceptions to other monads.
+  Add the auth and then send the notfication email. We return (Either Registration ())
+  because ultimately sending a notification returns no error, so we just return the unit
+  inside the monad.
+  TODO: rewrite this using the bind syntax
+-}
+register :: (AuthRepo m, EmailVerificationNotif m)
+         => Auth -> m (Either RegistrationError ())
+register auth = runExceptT $ do
+  vCode <- ExceptT $ addAuth auth
+  let email = authEmail auth
+  lift $ notifyEmailVerification email vCode
+
+-- Temporary code for testing, to be removed
+instance AuthRepo IO where
+  addAuth (Auth email pass) = do
+    putStrLn $ "adding auth: " <> rawEmail email
+    return $ Right "fake verification code"
+
+instance EmailVerificationNotif IO where
+  notifyEmailVerification email vcode =
+    putStrLn $ "Notify " <> rawEmail email <> " - " <> vcode
+
+-- End temporary code
+
+data EmailVerificationError = EmailVerificationErrorInvalidCode
+  deriving (Eq, Show)
+
+verifyEmail :: AuthRepo m 
+            => VerificationCode -> m (Either EmailVerificationError ())
+verifyEmail = setEmailAsVerified
+
+type UserId = Int
+
+type SessionId = Text
+
+data LoginError = LoginErrorInvalidAuth
+  | LoginErrorEmailNotVerified
+  deriving (Eq, Show)
+
+class Monad m => SessionRepo m where
+  newSession :: UserId -> m SessionId
+  findUserBySessionId :: SessionId -> m (Maybe UserId)
+
+resolveSessionId :: SessionRepo m => SessionId -> m (Maybe UserId)
+resolveSessionId = findUserBySessionId
+
+login :: (AuthRepo m, SessionRepo m)
+      => Auth -> m (Either LoginError SessionId)
+login auth = runExceptT $ do
+  result <- lift $ findUserByAuth auth
+  case result of
+    Nothing -> throwError LoginErrorInvalidAuth
+    Just (_, False) -> throwError LoginErrorEmailNotVerified
+    Just (uId, _) -> lift $ newSession uId
+
+getUser :: AuthRepo m => UserId -> m (Maybe Email)
+getUser = findEmailFromUserId
